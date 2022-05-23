49959e560188630ab7002ec249bd3c247afe2c74
sangminsong <ssmin940606@gmail.com>
2022-05-23 20:16:53 +0900

    refactor: import문 그룹화
    - 관련 피드백: https://github.com/woowacourse/react-shopping-cart/pull/115#discussion_r878966578

--- react-shopping-cart/src/pages/ProductListPage/ProductListPage.jsx
+++ react-shopping-cart/src/pages/ProductListPage/ProductListPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 import FlexWrapper from 'components/@shared/FlexWrapper/FlexWrapper';
