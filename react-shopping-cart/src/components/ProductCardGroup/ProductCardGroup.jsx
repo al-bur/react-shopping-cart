c19ba6817d17dce70ed1f6a105b7790723a5f237
sangminsong <ssmin940606@gmail.com>
2022-05-24 09:25:37 +0900

    refactor: pagination 컴포넌트 위치 변경
    - productCardGroup에 종속되어있는 pagination 컴포넌트 productCardGroup 컴포넌트 내부에 위치하도록 변경
    
    관련 피드백: https://github.com/woowacourse/react-shopping-cart/pull/115#discussion_r878971035

--- react-shopping-cart/src/components/ProductCardGroup/ProductCardGroup.jsx
+++ react-shopping-cart/src/components/ProductCardGroup/ProductCardGroup.jsx
@@ -1,31 +1,58 @@
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+
+import FlexWrapper from 'components/@shared/FlexWrapper/FlexWrapper';
+import Pagination from 'components/@shared/Pagination/Pagination';
+import WithSpinner from 'components/@shared/WithSpinner/WithSpinner';
 
 import ProductCard from 'components/ProductCard/ProductCard';
 
+import { fetchCartsStart } from 'redux/carts/carts.action';
 import { selectCurrentCarts } from 'redux/carts/carts.selector';
+import { fetchProductsStart } from 'redux/products/products.action';
 import { selectCurrentProducts } from 'redux/products/products.selector';
+import { selectIsProductsLoading } from 'redux/products/products.selector';
 
 import { isInCart } from 'utils/check';
 
 function ProductCardGroup() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsProductsLoading);
   const products = useSelector(selectCurrentProducts);
   const carts = useSelector(selectCurrentCarts);
 
+  const [page, setPage] = useState(localStorage.getItem('page') ?? 1);
+
+  const handlePage = (page) => {
+    setPage(page);
+
+    localStorage.setItem('page', page);
+  };
+
+  useEffect(() => {
+    dispatch(fetchProductsStart(page));
+    dispatch(fetchCartsStart());
+  }, [page]);
+
   return (
-    <Styled.Root>
-      {products.map((product) => {
-        return (
-          <ProductCard
-            key={product.id}
-            // THINK: isincart를 여기서 하는 게 맞을까? 여기서 하면 안될듯
-            $isincart={isInCart(product.id, carts)}
-            {...product}
-          />
-        );
-      })}
-    </Styled.Root>
+    <WithSpinner isLoading={isLoading}>
+      <FlexWrapper flexDirection="column" gap="60px">
+        <Styled.Root>
+          {products.map((product) => {
+            return (
+              <ProductCard
+                key={product.id}
+                // THINK: isincart를 여기서 하는 게 맞을까? 여기서 하면 안될듯
+                $isincart={isInCart(product.id, carts)}
+                {...product}
+              />
+            );
+          })}
+        </Styled.Root>
+        <Pagination page={page} handlePage={handlePage} />
+      </FlexWrapper>
+    </WithSpinner>
   );
 }
 
--- react-shopping-cart/src/pages/ProductListPage/ProductListPage.jsx
+++ react-shopping-cart/src/pages/ProductListPage/ProductListPage.jsx
@@ -1,25 +1,11 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-
-import FlexWrapper from 'components/@shared/FlexWrapper/FlexWrapper';
-import Pagination from 'components/@shared/Pagination/Pagination';
-import WithSpinner from 'components/@shared/WithSpinner/WithSpinner';
+import { useSelector } from 'react-redux';
 
 import ProductCardGroup from 'components/ProductCardGroup/ProductCardGroup';
 
-import { fetchCartsStart } from 'redux/carts/carts.action';
-import { fetchProductsStart } from 'redux/products/products.action';
-import {
-  selectProductsError,
-  selectIsProductsLoading,
-} from 'redux/products/products.selector';
+import { selectProductsError } from 'redux/products/products.selector';
 
 function ProductListPage() {
-  const dispatch = useDispatch();
-  const [searchParams] = useSearchParams();
-  const idx = searchParams.get('page') ?? 1;
-  const isLoading = useSelector(selectIsProductsLoading);
   const error = useSelector(selectProductsError);
 
   useEffect(() => {
@@ -28,20 +14,7 @@ function ProductListPage() {
     }
   }, [error]);
 
-  // THINK: withSpinner 때문에, ProductCardGroup에서 datafetching을 못함
-  useEffect(() => {
-    dispatch(fetchProductsStart(idx));
-    dispatch(fetchCartsStart());
-  }, [idx]);
-
-  return (
-    <WithSpinner isLoading={isLoading}>
-      <FlexWrapper flexDirection="column" gap="60px">
-        <ProductCardGroup />
-        <Pagination />
-      </FlexWrapper>
-    </WithSpinner>
-  );
+  return <ProductCardGroup />;
 }
 
 export default ProductListPage;
